6d8febd85213021651a8e6db930437140e5d8bd4
david <david3oliveros@gmail.com>
2022-10-20 11:31:34 -0400

    update database models

--- newsletter/models.py
+++ newsletter/models.py
@@ -5,7 +5,27 @@ from django.db import models
 
 class Issue(models.Model):
     title = models.CharField(max_length=200, null=True)
-    body = models.CharField(max_length=200, null=True)
+
+    first_impressions_robert_title = models.CharField(max_length=200, null=True)
+    first_impressions_robert_desc = models.CharField(max_length=200, null=True)
+    first_impressions_robert_body = models.TextField(blank=True)
+
+    first_impressions_stephen_title = models.CharField(max_length=200, null=True)
+    first_impressions_stephen_desc = models.CharField(max_length=200, null=True)
+    first_impressions_stephen_body = models.TextField(blank=True)
+
+    first_impressions_michael_title = models.CharField(max_length=200, null=True)
+    first_impressions_michael_desc = models.CharField(max_length=200, null=True)
+    first_impressions_michael_body = models.TextField(blank=True)
+
+    never_gets_old_title = models.CharField(max_length=200, null=True)
+    never_gets_old_desc = models.CharField(max_length=200, null=True)
+    never_gets_old_body = models.TextField(blank=True)
+
+    composer_spotlight_title = models.CharField(max_length=200, null=True)
+    composer_spotlight_desc = models.CharField(max_length=200, null=True)
+    composer_spotlight_body = models.TextField(blank=True)
+
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
 
