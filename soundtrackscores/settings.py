599158dd3c0ef55174784ac2d9fd7054c518d750
david <david3oliveros@gmail.com>
2022-10-27 15:29:07 -0400

    work on styling

--- soundtrackscores/settings.py
+++ soundtrackscores/settings.py
@@ -125,11 +125,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/media/'
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 ]
+
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
